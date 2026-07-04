3ed1efe3eab9c793a664e25cc0a3f6202cebc04b
Shivam Kumar <76731566+Shivam562-j@users.noreply.github.com>
2026-07-04 14:05:49 +0000

    add the url encoded, json, file and cookies setu

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
 
-connectDB();
+connectDB()
+    .then(() => {
+        const PORT = process.env.PORT || 8000;
+
+        const server = app.listen(PORT, () => {
+            console.log(`⚙️ Server is running on port ${PORT}`);
+        });
+
+        server.on("error", (error) => {
+            console.error("Server Error:", error);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGO db connection failed !!!", err);
+    })
