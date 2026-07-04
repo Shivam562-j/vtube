b7e1238273ad1df1b666974eda7820ebd71af851
Shivam Kumar <76731566+Shivam562-j@users.noreply.github.com>
2026-07-04 15:56:08 +0000

    make the user model in mongodb

---
+++ src/models/user.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true, 
+            lowercase: true, 
+            trim: true, 
+            index: true
+        }
+    }
+);
+
+
+export const User = mongoose.model("User", userSchema);
